39108dc47435ba6cda31cbd25a28b824cbdfd370
Kaiwen Jiang <64115446+kaiwenj@users.noreply.github.com>
2024-03-07 19:58:16 -0800

    Update vae.py

--- src/vae.py
+++ src/vae.py
@@ -1,4 +1,5 @@
 
+import torch
 import torch.nn as nn
 
 class VAE(nn.Module):
@@ -16,3 +17,13 @@ class VAE(nn.Module):
         xHat=self.decoder.decode(z)
         loss=self.calculateLoss(x, xHat, z_mu, log_z_sigma2)
         return loss
+
+def reparameterization(mean, var):
+    epsilon = torch.randn_like(var)#.to(device)
+    z = mean + var*epsilon
+    return z
+
+def calculateLoss(x, xHat, z_mu, log_z_sigma2):
+    reproductionLoss = nn.functional.binary_cross_entropy(xHat, x, reduction='sum')
+    dkl=-0.5*torch.sum(1+log_z_sigma2-z_mu.pow(2)-log_z_sigma2.exp())
+    return reproductionLoss+dkl
